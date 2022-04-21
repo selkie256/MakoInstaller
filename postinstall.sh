a6f5bf7c5d2b8656ee7816215d011ded4c4754fa
selkie256 <59953580+selkie256@users.noreply.github.com>
2022-04-21 15:51:10 +0200

    Add files via upload

--- postinstall.sh
+++ postinstall.sh
@@ -20,8 +20,8 @@ echo "Geben Sie einen Host-Namen an!"
 read HOSTNAME
 echo $HOSTNAME >> /etc/hostname
 mkinitcpio -P
-passwd
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Mermaidia OS"
+passwd
 grub-mkconfig -o /boot/grub/grub.cfg
 echo "Bitte geben Sie einen Benutzernamen für Ihren Benutzer ein!"
 read USERNAME
