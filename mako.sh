92d3b7ce11fb41d8d5c35e56a7c2d2faf4612081
selkie256 <59953580+selkie256@users.noreply.github.com>
2022-04-21 13:43:26 +0200

    Initial release

--- mako.sh
+++ mako.sh
@@ -83,30 +83,4 @@ then
 fi
 pacstrap /mnt base linux linux-firmware base-devel efibootmgr grub plasma-meta kde-applications libreoffice-fresh-de thunderbird vlc sddm nano sudo supertuxkart sauerbraten sauerbraten-data intel-ucode mesa wayland plasma-wayland-session kodi supertux codeblocks
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-echo "Bitte geben Sie ihre Zeitzone an! Z. B. Europe/Berlin"
-read TIMEZONE
-if [ -b /usr/share/zoneinfo/!$TIMEZONE ];
-then
-	while [ -b /usr/share/zoneinfo/$TIMEZONE ];
-	do
-		echo "Diese Zeitzone existiert nicht! Bitte geben Sie sie noch einmal ein!"
-		read TIMEZONE
-	done
-fi
-ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
-hwclock --systohc
-echo "de_DE.UTF8" >> /etc/locale.gen
-locale-gen
-echo "LANG=de_DE.UTF-8" >> /etc/locale.conf
-echo "KEYMAP=de-latin1" >> /etc/vconsole.conf
-echo "Geben Sie einen Host-Namen an!"
-read HOSTNAME
-echo $HOSTNAME >> /etc/hostname
-mkinitcpio -P
-passwd
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Mermaidia OS"
-grub-mkconfig -o /boot/grub/grub.cfg
-echo "Bitte geben Sie einen Benutzernamen für Ihren Benutzer ein!"
-read USERNAME
-useradd -m -G video,audio,storage,floppy,ftp,http,games,disk,kvm,network,scanner $USERNAME
+arch-chroot /mnt ./postinstall.sh
